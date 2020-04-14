b1e0036163d2d1bbb97eceaf2b892908fba55008
Lindomar Silva <lindomar1016@hotmail.com>
2020-04-14 16:50:09 -0300

    corrigido webpack vuejs

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,4 @@ const mix = require('laravel-mix');
 
 mix.setPublicPath('public_html/');
 mix.js('resources/js/app.js', 'js')
-   .sass('resources/sass/app.scss', 'css');
+   .sass('resources/sass/app.scss', 'css').version();
