ad149c7793936200e8b70ce82b4d7fad7a76231f
Lindomar Silva <lindomar1016@hotmail.com>
2020-04-14 16:53:31 -0300

    adc meta no cache painel

--- resources/views/ramodnil/master.blade.php
+++ resources/views/ramodnil/master.blade.php
@@ -8,6 +8,7 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
+	<meta http-equiv="pragma" content="no-cache" />
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/azzara.min.css') }}">
 	<link rel="stylesheet" href="{{asset('fonts/flaticon/flaticon.css')}}">
