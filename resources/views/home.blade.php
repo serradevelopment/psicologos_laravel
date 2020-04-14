a7c114607e0063eb4eccbf6fdb3ab39aca828127
Lindomar Silva <lindomar1016@hotmail.com>
2020-04-13 21:53:20 -0300

    adc video youtube

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -165,7 +165,7 @@
 							<div class="featured-box py-5">
 								<div class="featured-box-background" style="background-image: url(img/demos/seo/generic-2.jpg); background-size: cover; background-position: center;"></div>
 								<div class="box-content px-lg-4 px-xl-5 py-lg-5 m-5">
-									<a class="text-decoration-none lightbox" href="https://vimeo.com/45830194" data-plugin-options="{'type':'iframe'}">
+									<a class="text-decoration-none lightbox" href="https://www.youtube.com/watch?v=REyu5S9vhx8" data-plugin-options="{'type':'iframe'}">
 										<i class="fas fa-play featured-icon featured-icon-style-2 featured-icon-hover-effect-1 text-color-dark bg-light right-4 top-0 m-0 appear-animation" data-appear-animation="expandIn" data-appear-animation-delay="1200"></i>
 									</a>
 								</div>
