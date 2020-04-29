04dadb5fa3e9530ecb574179c638eef76ada3e64
Lindomar Silva <lindomar1016@hotmail.com>
2020-04-29 14:11:16 -0300

    adicionado termo de autorização para menores de idade

--- app/Http/Controllers/Admin/SchedulesUsersController.php
+++ app/Http/Controllers/Admin/SchedulesUsersController.php
@@ -54,7 +54,7 @@ class SchedulesUsersController extends Controller implements ShouldQueue
             $patient->fill($data['patient']);
             $patient->save();
 
-            // Mail::to($user->email)->send(new \App\Mail\ConsultaAgendada($user,$patient,$schedule,$data['date']));
+            Mail::to($user->email)->send(new \App\Mail\ConsultaAgendada($user,$patient,$schedule,$data['date']));
             $save = DB::insert('UPDATE schedules_has_users SET patients_id = ?, status = "SCHEDULED" WHERE schedules_id = ? and date = ? and users_id = ?', [
                 $patient->id, $data['schedule']['id'], $data['date'], $data['user']['id']
             ]);
