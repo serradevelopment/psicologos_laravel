37e1ef34daa897d96fce4bde8c78a66b543bcb44
Lindomar Silva <lindomar1016@hotmail.com>
2020-05-01 12:20:42 -0300

    corrigido paginação de ratings

--- app/Http/Controllers/Admin/RatingsController.php
+++ app/Http/Controllers/Admin/RatingsController.php
@@ -15,7 +15,7 @@ class RatingsController extends Controller
     
     public function index(){
         $allRatings = Rating::orderBy('created_at','desc')->get();
-        $ratings = Rating::orderBy('created_at','desc')->paginate(10);
+        $ratings = Rating::orderBy('created_at','desc')->paginate(9);
         return view('admin.ratings.index')->with(['ratings'=>$ratings,'allRatings'=>$allRatings]);
     }
 
